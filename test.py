ab57f9aed81bf7a0dac088ddddeac420102ef357
Jorikos <26204@ma-web.nl>
2022-05-19 14:39:33 +0200

    More testing

--- test.py
+++ test.py
@@ -1,7 +1,38 @@
-print('Hello world');
+import os
+import cmd
 
-a = 50
-b = 23
+y = 0
 
-if b < a:
-    print('b is greater than a\n' + 'a has a value of: ' + str(a) + '\n' + "b has a value of: " + str(b))
+#function
+# def Function(x):
+#    print(x) 
+#    y += 1
+
+#print
+print('Hello world')
+
+#variables
+while True:
+    try:
+        a = int(input("Enter a number: "))
+        b = int(input("Enter another number:"))
+        break
+    except:
+        print("Enter a number!")
+
+#if statement
+if b > a:
+    print('Number 2 is greater than number 1\n' + '2 has a value of: ' + str(b) + '\n' + '1 has a value of: ' + str(a))
+else:
+    print('Number 1 is greater than number 2\n' + '1 has a value of: ' + str(a) + '\n' + '2 has a value of: ' + str(b))
+
+#for loop
+for x in "Hello":
+    print(x)
+
+#list
+
+# Function("test")
+
+# while(y > 0):
+#    print(y)
